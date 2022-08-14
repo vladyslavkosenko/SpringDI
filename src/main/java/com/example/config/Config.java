26d366ba6f12ddb93c36c900a6b3d0f82bacecf9
vladyslavkosenko <vaduha91@gmail.com>
2022-08-14 20:57:25 +0300

    Java Config

--- src/main/java/com/example/config/Config.java
+++ src/main/java/com/example/config/Config.java
@@ -1,6 +1,8 @@
 package com.example.config;
 
 import com.example.project.Calculator;
+import com.example.project.MinusService;
+import com.example.project.PlusService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,16 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
     @Bean
     public Calculator calculator() {
-        return new Calculator();
+        return new Calculator(plusService(), minusService());
+    }
+
+    @Bean
+    public PlusService plusService() {
+        return new PlusService();
+    }
+
+    @Bean
+    public MinusService minusService() {
+        return new MinusService();
     }
 }
